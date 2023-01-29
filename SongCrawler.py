ad3b6017da7c4214d43c5377c6729179eaa790df
Kyle Androschuk <kandrosc@ualberta.ca>
2023-01-29 16:10:43 -0700

    Added download songs function

--- SongCrawler.py
+++ SongCrawler.py
@@ -5,7 +5,9 @@ import os, sys
 import json
 from multiprocessing import Process
 from bs4 import BeautifulSoup
+from pytube import YouTube
 FILEPATH = os.path.dirname(os.path.abspath(__file__)) + "/"
+with open(FILEPATH+"config.json","r") as f: config=json.loads(f.read())
 
 helpString = '''
 
@@ -15,12 +17,22 @@ Arguments:
 2 - Download selected albums with artist name / numeric input
 
 '''
-
+# Downloads two html files that have examples of the two types of containers that contain albums / songs
 def writeExamples():
         for i, artist in enumerate(['imagine dragons', 'july talk']):
             r = requests.get("https://www.google.com/search?q={}+albums".format(artist.replace(" ","+")))
             with open(FILEPATH+"example-%s%s.html" % ("no-"*i,"image"),"w") as f: f.write(r.text)
 
+# Downloads a song using the provided url, name (optional), convert it to .mp3, and move it to the music directory
+def downloadSong(url, name=""):
+    yt = YouTube(url)
+    streams = yt.streams.filter(only_audio=True)
+    # sort streams by abr
+    streams = sorted(streams, key=lambda x: int(x.abr.split("kbps")[0]))[::-1]
+    f = streams[0].download()
+    filename = (name if name else os.path.basename(f)[:-5]) + '.mp3'
+    os.system('ffmpeg -i "%s" -vn -ab 128k -ar 44100 -y "%s"' % (f,config['music_dir'] + '\\' + filename))
+    os.remove(f)
 
 if __name__ == "__main__":
 
@@ -35,13 +47,15 @@ if __name__ == "__main__":
                 if arg == 0:
                     writeExamples()
                 elif arg == 1:
-                    print(1)
+                    url = input("Enter the url of the song you would like to download: ")
+                    name = input("Enter the name you would like the file to be renamed to (optional): ")
+                    downloadSong(url, name)
                 else:
                     print(2) 
         except ValueError:
             print(helpString)
     '''
-    with open(FILEPATH+"config.json","r") as f: config=json.loads(f.read())
+    
     artist = input("Enter the name of the artist you would like to download music from: ")
     r = requests.get("https://www.google.com/search?q={}+albums".format(artist.replace(" ","+")))
     with open(FILEPATH+"test.html","w") as f: f.write(r.text)
